0892ec7900a5af18403d702869fca342787b88a1
theeugenechong <eugenechong20011@gmail.com>
2024-03-28 23:13:26 +0800

    Minor changes

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -15,7 +15,7 @@ const defaultProgram: string = "package main\n\nimport \"fmt\"\n\nfunc main() {\
 const outputPrompt: string = 'Click "Go!" to run your code!';
 
 export default function Editor() {
-    const [code, setCode] = useState<string>(defaultProgram)
+    const [code, setCode] = useState<string>(defaultProgram);
     const [output, setOutput] = useState<string>(outputPrompt);
 
     const compileAndRun = () => {
@@ -38,54 +38,54 @@ export default function Editor() {
     }
 
     return (
-        <div className="grid grid-cols-12 items-start gap-6 w-full mx-auto">
-            <div className="flex flex-col col-span-7">
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-lg font-bold" style={{ color: '#02dcff'}}>Code</CardTitle>
-                        <CardDescription className="text-sm">Run your Go code here.</CardDescription>
-                    </CardHeader>
-                    <CardContent className="p-8 mt-[-23px] h-[600px]">
-                        <AceEditor
-                            mode="golang"
-                            theme="solarized_dark"
-                            value={code}
-                            onChange={setCode}
-                            name="editor"
-                            width="100%"
-                            height="100%"
-                            fontSize={14}
-                            showPrintMargin={true}
-                            showGutter={true}
-                            highlightActiveLine={true}
-                            setOptions={aceOptions}
-                            className="!rounded-lg"
-                        />
-                    </CardContent>
-                </Card>
-                <div className="grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 mt-3">
-                    <Button className="!text-white font-bold transform active:scale-95 transition duration-75 !bg-sky-500 hover:bg-sky-700" onClick={compileAndRun}>
-                        <FaPlay className="mr-1" /> 
-                        Go!
-                    </Button>
-                    <Button className="!text-white font-bold transform active:scale-95 transition duration-75 !bg-sky-500 hover:bg-sky-700" onClick={resetCode}>
-                        <AiOutlineReload className="mr-1" />
-                        Reset
-                    </Button>
-                </div>
-            </div>
-            <div className="col-span-5">
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-lg font-bold" style={{ color: '#02dcff'}}>Output</CardTitle>
-                        <CardDescription className="text-sm">View compilation and execution output.</CardDescription>
-                    </CardHeader>
-                    <CardContent className="p-0 h-[400px]">
-                        <div style={{ height: 200 }} className="font-mono p-8 mt-[-20px]">{output}</div>
-                    </CardContent>
-                </Card>
-            </div>
+      <div className="grid grid-cols-12 items-start gap-6 w-full mx-auto">
+        <div className="flex flex-col col-span-7">
+          <Card>
+            <CardHeader className="ml-2">
+              <CardTitle className="text-lg font-bold" style={{ color: '#02dcff'}}>Code</CardTitle>
+              <CardDescription className="text-sm">Run your Go code here.</CardDescription>
+            </CardHeader>
+            <CardContent className="p-8 mt-[-23px] h-[600px]">
+              <AceEditor
+                  mode="golang"
+                  theme="solarized_dark"
+                  value={code}
+                  onChange={setCode}
+                  name="editor"
+                  width="100%"
+                  height="100%"
+                  fontSize={14}
+                  showPrintMargin={true}
+                  showGutter={true}
+                  highlightActiveLine={true}
+                  setOptions={aceOptions}
+                  className="!rounded-lg"
+              />
+            </CardContent>
+          </Card>
+          <div className="grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 mt-3">
+            <Button className="!text-white font-bold transform active:scale-95 transition duration-75 !bg-sky-500 hover:bg-sky-700" onClick={compileAndRun}>
+              <FaPlay className="mr-1" /> 
+              Go!
+            </Button>
+            <Button className="!text-white font-bold transform active:scale-95 transition duration-75 !bg-sky-500 hover:bg-sky-700" onClick={resetCode}>
+              <AiOutlineReload className="mr-1" />
+              Reset
+            </Button>
+          </div>
         </div>
+        <div className="col-span-5">
+          <Card>
+            <CardHeader>
+              <CardTitle className="text-lg font-bold" style={{ color: '#02dcff'}}>Output</CardTitle>
+              <CardDescription className="text-sm">View compilation and execution output.</CardDescription>
+            </CardHeader>
+            <CardContent className="p-0 h-[400px]">
+              <div style={{ height: 200 }} className="font-mono p-8 mt-[-20px]">{output}</div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     )
 }
 
