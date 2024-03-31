885bdb308ae44cc5c75ed4d79d0c6e29588e9bb0
theeugenechong <eugenechong20011@gmail.com>
2024-03-31 21:19:19 +0800

    fix favicon issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "GoCurrent",
+  description: "GoCurrent is a Go playground for running Go code in the browser.",
+  icons: {
+    icon: "/go-redeyes.png",
+  }
 };
 
 export default function RootLayout({
@@ -18,10 +22,8 @@ export default function RootLayout({
     <>
       <html lang="en">
         <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="description"/>
-          <title>GoCurrent</title>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" href="/go-redeyes.png" />
         </head>
         <body className={inter.className}>{children}</body>
